7b479e5b0572f9d33c0460b1111d560b64c93fca
Halcik <julkatablet2507@gmail.com>
2022-11-18 18:54:34 +0100

    Delete comments

--- pomodoro.py
+++ pomodoro.py
@@ -1,31 +1,16 @@
-#lista zadań do wykonania wprowadzana na początku i potem odhaczanie? Każde, by się dało zrobić w 25 min
-
-#ustawienie prorytetów - jakieś przesuwanie?
-
-#odznaczenie wykonanych zadań
-
-#sztuczne kroki przygotowawcze:
-# - wyłączenie powiadomień
-# - wyciszenie telefonu i wyłączenie wibracji
-# - wyłączenie niepotrzebnych stron i aplikacji
-
 from datetime import timedelta, datetime
 from playsound import playsound
 
 def pomodoro():
-    #timer odlicza 25 minutowe odcinki czasu pracy - jakaś muzyka w tle?
     print("Czas na naukę!")
     p_beg = datetime.today()
     p_different = timedelta(minutes=25)
     p_close = p_beg
     while p_close-p_beg < p_different:
-        #może w trakcie pomodoro jakieś teksty motywujące?
         p_close = datetime.today()
-    #potem krótka przerwa - dzwonek na przerwę?
 
 
 def short_break():
-    #pierwsza przerwa - 5 min - czas na spacer po mieszkaniu, przewietrzenei czy nalanie wody
     print("Przerwa!")
     b_beg = datetime.today()
     b_different = timedelta(minutes=5)
@@ -35,7 +20,6 @@ def short_break():
     
 
 def long_break():
-    #Co czwarte pomodoro dłuższa przerwa - 15-30 min - dobre na zrobienie kawy lub herbaty, zjedzenie czy rozmowę ze znajomymi.
     print("Pora na dłuższy odpoczynek")
     lb_beg = datetime.today()
     lb_different = timedelta(minutes=30)
